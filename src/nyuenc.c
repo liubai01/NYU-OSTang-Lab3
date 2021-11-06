6e7c9871730f271226a078ed1fe634174c744d29
yintaoxu <yintao.xu@nyu.edu>
2021-11-06 15:41:34 -0400

    sequential version

--- src/nyuenc.c
+++ src/nyuenc.c
@@ -1,7 +1,50 @@
 #include <stdio.h>
 
-int main()
+
+int main(int argc, char* argv[])
 {
-	printf("Hello world!\n");
-	return 0;
+    FILE *fp = NULL;
+    char c;
+    int accChar = -1; // accmulated character
+    int cnt = 0; // counter of occurence of character
+
+    for (int fidx = 1; fidx < argc; ++fidx)
+    {
+        fp = fopen(argv[fidx], "r");
+
+        if (!fp) {
+            printf("File %s not exists!", argv[fidx]);
+        }
+        
+        // read character by character
+        while ((c = fgetc(fp)) != EOF)
+        {
+          if (accChar != c) {
+            if (cnt) {
+                printf("%c%c", accChar, cnt);
+            }
+            accChar = c;
+            cnt = 1;
+          } else {
+            ++cnt;
+            // reset counter when reach 16
+            if (cnt == 16)
+            {
+                printf("%c%c", accChar, cnt);
+                accChar = -1;
+                cnt = 0;
+            }
+
+          }
+        }
+
+        fclose(fp);
+    }
+
+    // output the tail
+    if (cnt) {
+        printf("%c%c", accChar, cnt);
+    }
+    
+    return 0;
 }
